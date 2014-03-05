b09e4d2d0211d4c5fed0a57031a4a17f36f43804
deathcap <deathcap@gmx.co.uk>
2014-03-04 19:14:07 -0800

    Expand frame info

--- index.js
+++ index.js
@@ -16,16 +16,39 @@ var parseMcmeta = function(image, data) {
   var tileWidth = image.width / animationWidth;
   var tileHeight = image.height / animationHeight;
 
+  var frameCoords = [];
+
   for (var j = 0; j < animationWidth; j += 1) {
-    for (var i = 0; i < animationHeight; i += 1) {
+    for (var i = 0; i < animationHeight; i += 1) { // assuming counted vertical then horizontal (not verified)
       var sx = j * tileWidth;
       var sy = i * tileHeight;
       var ex = (j + 1) * tileWidth;
       var ey = (i + 1) * tileHeight;
 
-      console.log('frame (',sx,sy,') - (',ex,ey,')');
+      frameCoords.push([sx, sy, ex, ey]);
     }
   }
+
+  var frames = [];
+
+  for (i = 0; i < json.animation.frames.length; i += 1) {
+    var frameInfo = json.animation.frames[i];
+
+    var index = (typeof frameInfo === 'number') ? frameInfo : frameInfo.index;
+    var time = (typeof frameInfo === 'object' && 'time' in frameInfo) ? frameInfo.time : 1;
+
+    if (typeof frameInfo === 'number') {
+      index = frameInfo;
+    } else {
+      index = frameInfo.index;
+    }
+
+    var coords = frameCoords[index];
+
+    frames.push({index:index, time:time, coords:coords}); // TODO: crop image
+  }
+
+  console.log(frames);
 };
 
 var image = {width:16, height:16*7}; // TODO: real image
