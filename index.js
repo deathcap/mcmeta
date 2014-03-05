25cbf657f7e22f7b971a8c71bfea9fa420547705
deathcap <deathcap@gmx.co.uk>
2014-03-04 20:07:23 -0800

    Return only one tile if not animated

--- index.js
+++ index.js
@@ -13,13 +13,11 @@ var parseMcmeta = function(image, data) {
 
   console.log(json);
 
-  if (!json.animation) return;
-
   // number of frames horizontal and vertical
-  var animationWidth = json.animation.width || 1;
-  var animationHeight = json.animation.height || (image.height / image.width); // vertical strip of frames
+  var animationWidth = (json.animation && json.animation.width) || 1;
+  var animationHeight = (json.animation && json.animation.height) || (image.height / image.width); // vertical strip of frames
 
-  var defaultFrametime = json.animation.frametime || 1;
+  var defaultFrametime = (json.animation && json.animation.frametime) || 1;
 
   var tileWidth = image.width / animationWidth;
   var tileHeight = image.height / animationHeight;
@@ -38,43 +36,52 @@ var parseMcmeta = function(image, data) {
   }
 
   var frames = [];
+  if (json.animation) {
+    var frameInfos = json.animation.frames || upTo(animationWidth * animationHeight);
 
-  var frameInfos = json.animation.frames || upTo(animationWidth * animationHeight);
+    for (i = 0; i < frameInfos.length; i += 1) {
+      var frameInfo = frameInfos[i];
 
-  for (i = 0; i < frameInfos.length; i += 1) {
-    var frameInfo = frameInfos[i];
+      var index = (typeof frameInfo === 'number') ? frameInfo : frameInfo.index;
+      var time = (typeof frameInfo === 'object' && 'time' in frameInfo) ? frameInfo.time : 1;
 
-    var index = (typeof frameInfo === 'number') ? frameInfo : frameInfo.index;
-    var time = (typeof frameInfo === 'object' && 'time' in frameInfo) ? frameInfo.time : 1;
+      if (typeof frameInfo === 'number') {
+        index = frameInfo;
+      } else {
+        index = frameInfo.index;
+      }
 
-    if (typeof frameInfo === 'number') {
-      index = frameInfo;
-    } else {
-      index = frameInfo.index;
+      var coords = frameCoords[index];
+
+      frames.push({index:index, time:time, coords:coords}); // TODO: crop image
     }
-
-    var coords = frameCoords[index];
-
-    frames.push({index:index, time:time, coords:coords}); // TODO: crop image
+  } else {
+    frames.push({coords:[0, 0, tileWidth, tileHeight]}); // TODO: or full texture?
   }
 
   console.log(frames);
 };
 
-var image = {width:16, height:16*7}; // TODO: real image
-parseMcmeta(image, JSON.stringify(
+parseMcmeta({width:16, height:16*7}, JSON.stringify(
 // http://minecraft.gamepedia.com/Resource_pack#Animation_Properties
 { 
    "animation":{
       "width":1,
       "height":7,
       "frametime":1,
-      /*"frames":[{
+      "frames":[{
          "index":0,
          "time": 0
          },
          1,2,3,4,5,6,4,2]
-         */
    }
 }));
 
+parseMcmeta({width:16, height:16*7}, JSON.stringify(
+{
+  "animation":{}
+}));
+
+parseMcmeta({width:16, height:16*7}, JSON.stringify(
+{
+}));
